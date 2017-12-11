751dd18b456ee526031f7be1290b5179b88f114b
Ahmed El Rheddane <ahmedelrheddane@gmail.com>
2017-12-11 20:28:52 +0100

    Added deadline filter.

--- dbjobs.js
+++ dbjobs.js
@@ -1,8 +1,10 @@
-rows = document.getElementsByTagName('tbody');
-rows_length = rows.length
+var rows = document.getElementsByTagName('tbody');
+var rows_length = rows.length;
+var today = new Date();
 for (var i = 0; i < rows_length; ++i) {
-    var type = rows[i].children[0].children[1].innerHTML
-    if (type != 'job ann. ') {
+    var type = rows[i].children[0].children[1].innerHTML;
+    var date = new Date(rows[i].children[0].children[4].innerHTML);
+    if (type != 'job ann. ' || date < today) {
         rows[i].style.display = 'none';
     }
 }
